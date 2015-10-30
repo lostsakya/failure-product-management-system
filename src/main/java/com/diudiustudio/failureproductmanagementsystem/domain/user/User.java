93c4c7bff1125a5463e47549989f1b70a72a9cf2
liuhongjian <liuhongjian@ihbaby.com>
2015-10-30 16:28:52 +0800

    fix bugs

---
+++ src/main/java/com/diudiustudio/failureproductmanagementsystem/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.diudiustudio.failureproductmanagementsystem.domain.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by liuhongjian on 15/10/30.
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
