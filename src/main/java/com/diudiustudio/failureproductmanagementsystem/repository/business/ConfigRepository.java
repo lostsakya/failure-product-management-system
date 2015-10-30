f0182cb5d0e09889bad615cb47103bec7a09b42a
liuhongjian <liuhongjian@ihbaby.com>
2015-10-30 13:53:10 +0800

    fix bugs

--- src/main/java/com/diudiustudio/failureproductmanagementsystem/repository/business/ConfigRepository.java
+++ src/main/java/com/diudiustudio/failureproductmanagementsystem/repository/business/ConfigRepository.java
@@ -1,6 +1,7 @@
 package com.diudiustudio.failureproductmanagementsystem.repository.business;
 
 import com.diudiustudio.failureproductmanagementsystem.domain.business.Config;
+import com.diudiustudio.failureproductmanagementsystem.domain.business.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -9,6 +10,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(collectionResourceRel = "station", path = "station")
 public interface ConfigRepository extends JpaRepository<Config, Long> {
-//    Model findByConfigCn(String typeEn);
-//    Model findByConfigEn(String typeEn);
+    Model findByNameCn(String typeEn);
+    Model findByNameEn(String typeEn);
 }
