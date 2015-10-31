bec92b9e9d3d09ac4b6b1fd9beacc5d816aebc2f
james <hj.liu118@gmail.com>
2015-10-31 08:20:46 +0800

    fix bugs. add user domain and service

---
+++ src/main/java/com/diudiustudio/failureproductmanagementsystem/service/UserService.java
@@ -0,0 +1,10 @@
+package com.diudiustudio.failureproductmanagementsystem.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sakya on 2015/10/29.
+ */
+@Service
+public class UserService {
+}
